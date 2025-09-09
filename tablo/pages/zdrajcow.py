bb62c8df2a7046477071ea523d3c370c65be61cb
Pawel Drzyzga <pawel.drzyzga@comarch.com>
2025-09-09 13:26:46 +0200

    zdrajcowupd

--- tablo/pages/zdrajcow.py
+++ tablo/pages/zdrajcow.py
@@ -1,29 +1,38 @@
 import streamlit as st
 import os
+import re
 
+base_dir = os.path.join(os.getcwd(), 'tablo', 'zdrajcow')
+if not os.path.isdir(base_dir):
+    st.error(f"Directory not found: {base_dir}")
+    st.stop()
 
-files = os.listdir(os.getcwd() + '/tablo/zdrajcow/')
+# only keep common image extensions
+files = [f for f in os.listdir(base_dir) if re.search(r'\.(jpg|jpeg|png|gif)$', f, re.I)]
 
 members = {}
 for i in files:
-    member = os.path.splitext(i)[0].split('_')
-    members[member[0]] = member[1], i, int(member[0])%3
+    name_part, _ = os.path.splitext(i)
+    parts = name_part.split('_', 1)
+    if len(parts) != 2:
+        # skip files that don't match '<id>_<name>.<ext>'
+        continue
+    member_id, member_name = parts
+    try:
+        column = int(member_id) % 3
+    except ValueError:
+        # skip non-numeric ids
+        continue
+    members[member_id] = (member_name, i, column)
 
 col0, col1, col2 = st.columns([3,2,3])
 
 with col1:
     st.title('Tablo zdrajców')
 
-for id, (name, filename, column) in members.items():
-    # st.image('zdrajcow/' + filename, caption=name)
-    if column == 0:
-        col0, col1, col2 = st.columns(3)
-    if column == 0:
-        with col0:
-            st.image('tablo/zdrajcow/' + filename, caption=name, use_container_width=True)
-    elif column == 1:
-        with col1:
-            st.image('tablo/zdrajcow/' + filename, caption=name, use_container_width=True)
-    else:
-        with col2:
-            st.image('tablo/zdrajcow/' + filename, caption=name, use_container_width=True)
+cols = (col0, col1, col2)
+
+# iterate in numeric order of ids
+for member_id, (name, filename, column) in sorted(members.items(), key=lambda x: int(x[0])):
+    with cols[column]:
+        st.image(os.path.join('tablo', 'zdrajcow', filename), caption=name, use_container_width=True)
