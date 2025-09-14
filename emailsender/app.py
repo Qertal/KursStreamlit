e05d4e201b17c2ece98a6ea5f7b3f865f7fe1afd
Qertal <qertal123@gmail.com>
2025-09-14 08:19:02 +0200

    update

--- emailsender/app.py
+++ emailsender/app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.write("DB username:", st.secrets["db_username"])
-st.write("DB password:", st.secrets["db_password"])
+st.write("DB username:", st.secrets["DB_USERNAME"])
+st.write("DB password:", st.secrets["DB_PASSWORD"])
