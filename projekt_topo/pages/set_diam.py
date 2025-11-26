ea91667cc1014c22b157eac3ca409b415da799bf
Qertal <148963005+Qertal@users.noreply.github.com>
2025-11-26 12:34:54 +0000

    update diam

--- projekt_topo/pages/set_diam.py
+++ projekt_topo/pages/set_diam.py
@@ -45,7 +45,8 @@ if st.session_state.get('fields_generated', False):
                     points[i, k] = st.number_input(
                         label=f"Wspolrzedna {k + 1}",
                         key=f'coord_{i}_{k}',
-                        value=random.randint(-15, 15)
+                        value=random.randint(-15, 15),
+                        step=.01,
                     )
 
         submitted = st.form_submit_button(label="Oblicz")
